f9ca887b13a6acf8b3ab0ff311aa50e7439550a9
suneelseelam <suneelseelam@tridentnets.in>
2017-12-06 15:31:46 +0530

    Added all constant files that can be used in all the projects.

---
+++ ConstantFramework/Classes/AlertView/AlertViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AlertView.swift
+//  ConstantFramework
+//
+//  Created by Apple on 01/12/17.
+//
+
+import Foundation
+import UIKit
+
+public class AlertViewController : NSObject{
+    
+    public static let instance = AlertViewController()
+    
+    public func showOkCancelAlertView(title : String , Message : String, okActionTitle : String, cancleActionTitle : String, okAction: @escaping (_ action :UIAlertAction) -> Void , cancelAction: @escaping (_ action :UIAlertAction) -> Void) -> UIAlertController{
+        let dialogMessage = UIAlertController(title: title, message: Message , preferredStyle: .alert)
+        
+        // Create OK button with action handler
+        let ok = UIAlertAction(title: "OK", style: .default, handler: okAction)
+        // Create Cancel button with action handler
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel,handler: cancelAction)
+        //Add OK and Cancel button to dialog message
+        dialogMessage.addAction(ok)
+        dialogMessage.addAction(cancel)
+        return dialogMessage
+    }
+    
+    public func showOkAlertView(title : String , Message : String, okActionTitle : String, okAction: @escaping (_ action :UIAlertAction) -> Void) -> UIAlertController{
+        let dialogMessage = UIAlertController(title: title, message: Message , preferredStyle: .alert)
+        
+        // Create OK button with action handler
+        let ok = UIAlertAction(title: okActionTitle, style: .default, handler: okAction)
+        //Add OK button to dialog message
+        dialogMessage.addAction(ok)
+        return dialogMessage
+    }
+    
+}
+
